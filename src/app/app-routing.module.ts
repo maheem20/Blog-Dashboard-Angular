9d1c638e3103e19904b4a8a58bfd0c9f0ff6e9a5
Maheem <88165826+maheem20@users.noreply.github.com>
2023-09-28 21:29:33 +0000

    add route to map '/categories' URL to CategoriesComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoriesComponent } from './categories/categories.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path: '', component : DashboardComponent }
+  { path: '', component : DashboardComponent },
+  { path: 'categories', component : CategoriesComponent }
 ];
 
 @NgModule({
