87ee39afae347523cc586bbf076d8857fe2b9620
maheem_20 <mwistanbuly@gmail.com>
2023-09-30 23:32:19 +0200

    move query code to categories service

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-categories',
@@ -8,7 +7,7 @@ import { AngularFirestore } from '@angular/fire/firestore';
 })
 export class CategoriesComponent implements OnInit {
 
-  constructor(private afs: AngularFirestore) { }
+  constructor() { }
 
   ngOnInit(): void {
   }
@@ -20,26 +19,5 @@ export class CategoriesComponent implements OnInit {
     let subCategoryData = {
       subCategory: 'subCategory1'
     }
-
-    this.afs.collection('categories').add(categoryData).then((docRef: { id: any; }) => {
-      console.log(docRef);
-
-      // this.afs.doc(`categories/${docRef.id}`).collection('subCategories').add(subCategoryData)
-      this.afs.collection('categories').doc(docRef.id).collection('subCategories').add(subCategoryData).then((docRef1: { id: any; }) => {
-        console.log(docRef1);
-
-        // this.afs.doc(`categories/${docRef.id}/subCategories/${docRef1.id}`).collection('subSubCategories').add(subCategoryData)
-
-        this.afs.collection('categories').doc(docRef.id).collection('subCategories').doc(docRef1.id).collection('subSubCategories').add(subCategoryData).then((docRef2: any) => {
-          console.log('Second level sub category successfully added!');
-        }).catch((err: any) => {
-          console.log(err)
-        }
-        );
-      });
-
-    }).catch((err: any) => {
-      console.log(err)
-    });
   }
 }
--- src/app/services/categories.service.ts
+++ src/app/services/categories.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriesService {
 
-  constructor() { }
+  constructor(private afs: AngularFirestore) { }
+
+  saveData() {
+    this.afs.collection('categories').add(categoryData).then((docRef: { id: any; }) => {
+      console.log(docRef);
+
+      // this.afs.doc(`categories/${docRef.id}`).collection('subCategories').add(subCategoryData)
+      this.afs.collection('categories').doc(docRef.id).collection('subCategories').add(subCategoryData).then((docRef1: { id: any; }) => {
+        console.log(docRef1);
+
+        // this.afs.doc(`categories/${docRef.id}/subCategories/${docRef1.id}`).collection('subSubCategories').add(subCategoryData)
+
+        this.afs.collection('categories').doc(docRef.id).collection('subCategories').doc(docRef1.id).collection('subSubCategories').add(subCategoryData).then((docRef2: any) => {
+          console.log('Second level sub category successfully added!');
+        }).catch((err: any) => {
+          console.log(err)
+        }
+        );
+      });
+
+    }).catch((err: any) => {
+      console.log(err)
+    });
+  }
 }
