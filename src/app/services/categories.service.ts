ea0bcc3f7ddfa0566e9cefd507ac244dfca250d4
maheem_20 <mwistanbuly@gmail.com>
2023-09-30 23:52:27 +0200

    remove sub collection level category queries

--- src/app/services/categories.service.ts
+++ src/app/services/categories.service.ts
@@ -5,9 +5,6 @@ import { AngularFirestore } from '@angular/fire/firestore';
   providedIn: 'root'
 })
 
-let subCategoryData = {
-  subCategory: 'subCategory1'
-}
 export class CategoriesService {
 
   constructor(private afs: AngularFirestore) { }
@@ -15,21 +12,6 @@ export class CategoriesService {
   saveData(data: any) {
     this.afs.collection('categories').add(data).then((docRef: { id: any; }) => {
       console.log(docRef);
-
-      // this.afs.doc(`categories/${docRef.id}`).collection('subCategories').add(subCategoryData)
-      this.afs.collection('categories').doc(docRef.id).collection('subCategories').add(subCategoryData).then((docRef1: { id: any; }) => {
-        console.log(docRef1);
-
-        // this.afs.doc(`categories/${docRef.id}/subCategories/${docRef1.id}`).collection('subSubCategories').add(subCategoryData)
-
-        this.afs.collection('categories').doc(docRef.id).collection('subCategories').doc(docRef1.id).collection('subSubCategories').add(subCategoryData).then((docRef2: any) => {
-          console.log('Second level sub category successfully added!');
-        }).catch((err: any) => {
-          console.log(err)
-        }
-        );
-      });
-
     }).catch((err: any) => {
       console.log(err)
     });
