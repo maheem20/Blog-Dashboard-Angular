134b7851aab4ef28dddefdff2e02dfebb54c4b94
maheem_20 <mwistanbuly@gmail.com>
2023-10-01 22:54:11 +0200

    define loadData function to read and Retrieve categories from Cloud Firestore

--- src/app/services/categories.service.ts
+++ src/app/services/categories.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { ToastrService } from "ngx-toastr";
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -18,4 +19,6 @@ export class CategoriesService {
       console.log(err)
     });
   }
+
+  loadData() { }
 }
