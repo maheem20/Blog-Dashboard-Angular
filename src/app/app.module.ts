0b8d7fdbfb73559092c36a1704093616f3d3c766
maheem_20 <mwistanbuly@gmail.com>
2023-10-01 22:26:03 +0200

    add the @angular/fire package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,7 @@ import { ToastrModule } from "ngx-toastr";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AngularFireModule } from '@angular/fire';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
-
-import { environment } from '../environments/environment.prod';
+import { AngularFirestoreModule, provideFirestore, getFirestore } from '@angular/fire/firestore';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,6 +13,11 @@ import { HeaderComponent } from './layouts/header/header.component';
 import { FooterComponent } from './layouts/footer/footer.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CategoriesComponent } from './categories/categories.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideDatabase, getDatabase } from '@angular/fire/database';
+import { provideStorage, getStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [
@@ -31,7 +34,12 @@ import { CategoriesComponent } from './categories/categories.component';
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFirestoreModule,
     ToastrModule.forRoot(),
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'angular-blog-b07da',
+    appId: '1:153621353491:web:77339089920112b50f1be6',
+    storageBucket: 'angular-blog-b07da.appspot.com',
+    apiKey: 'AIzaSyDjmwsEQ2KLW4O3ELFhp0QIsNVp79R8Iw0',
+    authDomain: 'angular-blog-b07da.firebaseapp.com',
+    messagingSenderId: '153621353491',
+  },
     production: false
 };
