8e5c4b33e254abb3b91f30122c27d4bd1fc697fe
maheem_20 <mwistanbuly@gmail.com>
2023-10-02 23:06:12 +0200

    make code more concise by using string template literal to specify path to document

--- src/app/services/categories.service.ts
+++ src/app/services/categories.service.ts
@@ -33,13 +33,13 @@ export class CategoriesService {
   }
 
   updateData(id: any, EditData: any) {
-    this.afs.collection('categories').doc(id).update(EditData).then(docRef => {
+    this.afs.doc(`categories/${id}`).update(EditData).then(docRef => {
       this.toastr.success('Category updated successfully!');
     })
   }
 
   deleteData(id: any) {
-    this.afs.collection('categories').doc(id).delete().then(docRef => {
+    this.afs.doc(`categories/${id}`).delete().then(docRef => {
       this.toastr.success('Category deleted successfully!');
     })
   }
