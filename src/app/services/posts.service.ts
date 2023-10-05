75e7e2452567d2d7f9e271a620c085e0027f5927
maheem_20 <mwistanbuly@gmail.com>
2023-10-05 23:55:22 +0200

    get download URL for uploaded image and log it to console

--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -8,12 +8,17 @@ export class PostsService {
 
   constructor(private storage: AngularFireStorage) { }
 
-  uploadImage(selectedImage: any) {
+  uploadImage(selectedImage: any, postData: any) {
     const filePath = `postIMG/${Date.now()}`;
     console.log(filePath);
 
     this.storage.upload(filePath, selectedImage).then(() => {
       console.log('Post image uploaded successfully!');
+
+      this.storage.ref(filePath).getDownloadURL().subscribe(URL => {
+        console.log(URL);
+        console.log(postData);
+      });
     });
   }
 }
