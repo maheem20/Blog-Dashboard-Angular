347b8424e440e861b8dcb436b1dbb3bff17627f0
maheem_20 <mwistanbuly@gmail.com>
2023-10-06 17:06:41 +0200

    use RxJS map operator to transform snapshotChanges observable and to transform each document change action

--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
 import { ToastrService } from 'ngx-toastr';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -37,7 +38,11 @@ export class PostsService {
   }
 
   loadData() {
-    return this.afs.collection('posts').snapshotChanges();
+    return this.afs.collection('posts').snapshotChanges().pipe(
+      map(actions => {
+        return actions.map(a => { });
+      })
+    );
   }
   
 }
