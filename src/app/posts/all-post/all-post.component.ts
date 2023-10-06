56fc63ff38de16b4de9e5a26d57c81e198e3bcbd
maheem_20 <mwistanbuly@gmail.com>
2023-10-06 17:09:14 +0200

    inject PostsService into constructor

--- src/app/posts/all-post/all-post.component.ts
+++ src/app/posts/all-post/all-post.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PostsService } from 'src/app/services/posts.service';
 
 @Component({
   selector: 'app-all-post',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./all-post.component.css']
 })
 export class AllPostComponent {
+
+  constructor(private postService: PostsService) { }
 
 }
