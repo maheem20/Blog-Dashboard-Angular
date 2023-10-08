6d7c7002a270f06a8f5b4e6b969e5752938a4e7d
maheem_20 <mwistanbuly@gmail.com>
2023-10-08 22:45:27 +0200

    if ID parameter is present, load data for post else, initialize post form with empty values

--- src/app/posts/new-post/new-post.component.ts
+++ src/app/posts/new-post/new-post.component.ts
@@ -35,34 +35,38 @@ export class NewPostComponent implements OnInit {
     this.router.queryParams.subscribe(val => {
 
       this.docId = val['id'];
-      this.postService.loadOneData(val['id']).subscribe(post => {
 
-        this.post = post;
+      if (this.docId) {
+        this.postService.loadOneData(val['id']).subscribe(post => {
 
-        this.postForm = this.fb.group({
-          title: [this.post.title, [Validators.required, Validators.minLength(10)]],
-          permalink: [this.post.permalink, Validators.required],
-          excerpt: [this.post.excerpt, [Validators.required, Validators.minLength(50)]],
-          category: [`${this.post.category.categoryId}-${this.post.category.category}`, Validators.required],
-          postImg: ['', Validators.required],
-          content: [this.post.content, Validators.required]
+          this.post = post;
+
+          this.postForm = this.fb.group({
+            title: [this.post.title, [Validators.required, Validators.minLength(10)]],
+            permalink: [this.post.permalink, Validators.required],
+            excerpt: [this.post.excerpt, [Validators.required, Validators.minLength(50)]],
+            category: [`${this.post.category.categoryId}-${this.post.category.category}`, Validators.required],
+            postImg: ['', Validators.required],
+            content: [this.post.content, Validators.required]
+          });
+
+          this.imgSrc = this.post.postImgPath;
+          this.formStatus = 'Edit';
+
         });
-
-        this.imgSrc = this.post.postImgPath;
-        this.formStatus = 'Edit';
-
-      });
+      } else {
+        this.postForm = this.fb.group({
+          title: ['', [Validators.required, Validators.minLength(10)]],
+          permalink: ['', Validators.required],
+          excerpt: ['', [Validators.required, Validators.minLength(50)]],
+          category: ['', Validators.required],
+          postImg: ['', Validators.required],
+          content: ['', Validators.required]
+        });
+      }
 
     });
 
-    this.postForm = this.fb.group({
-      title: ['', [Validators.required, Validators.minLength(10)]],
-      permalink: ['', Validators.required],
-      excerpt: ['', [Validators.required, Validators.minLength(50)]],
-      category: ['', Validators.required],
-      postImg: ['', Validators.required],
-      content: ['', Validators.required]
-    });
   }
 
   ngOnInit(): void {
