ca1972987f150f440567c876176340405f0bc352
maheem_20 <mwistanbuly@gmail.com>
2023-10-10 23:52:49 +0200

    inject ToastrService into constructor

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private afAuth: AngularFireAuth) { }
+  constructor(private afAuth: AngularFireAuth, private toastr: ToastrService) { }
 }
