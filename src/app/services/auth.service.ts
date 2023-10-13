074333f748143a3b2a6b1a800e5e73981db0ab38
Maheem <88165826+maheem20@users.noreply.github.com>
2023-10-13 21:14:18 +0000

    initialize BehaviorSubject with value of false

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,11 +2,14 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+
+  loggedIn = BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
   constructor(private afAuth: AngularFireAuth, private toastr: ToastrService, private router: Router) { }
 
