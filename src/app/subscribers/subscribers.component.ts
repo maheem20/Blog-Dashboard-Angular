ade1dead216b764950a086b7b618713175935514
maheem_20 <mwistanbuly@gmail.com>
2024-01-22 23:56:49 +0100

    inject SubscribersService and initialize subscribersArray

--- src/app/subscribers/subscribers.component.ts
+++ src/app/subscribers/subscribers.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SubscribersService } from '../services/subscribers.service';
 
 @Component({
   selector: 'app-subscribers',
   templateUrl: './subscribers.component.html',
   styleUrls: ['./subscribers.component.css']
 })
-export class SubscribersComponent {
+export class SubscribersComponent implements OnInit {
+
+  subscribersArray: Array<object> = [];
+
+  constructor(private subService: SubscribersService) { }
+
+  ngOnInit(): void {
+
+    this.subService.loadData().subscribe((val: any) => {
+      this.subscribersArray = val;
+    });
+
+  }
 
 }
