e32b6f42ab8605f50c07a8e138a8a23fd5c06aa1
maheem_20 <mwistanbuly@gmail.com>
2024-01-22 23:52:00 +0100

    define loadData to load data from Firestore

--- src/app/services/subscribers.service.ts
+++ src/app/services/subscribers.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SubscribersService {
 
-  constructor() { }
+  constructor(private afs: AngularFirestore) { }
+
+  loadData() {
+    return this.afs.collection('subscribers').snapshotChanges().pipe(
+      map(actions => {
+        return actions.map(a => {
+          const data: any = a.payload.doc.data();
+          const id = a.payload.doc.id;
+          return { id, ...data };
+        });
+      })
+    );
+  }
+
 }
